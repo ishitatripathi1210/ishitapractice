eb791c3788ad0e72c77f376af77952038d960206
ishitatripathi1210 <106306979+ishitatripathi1210@users.noreply.github.com>
2022-05-26 22:39:08 +0530

    Create prac_ques.py

---
+++ prac_ques.py
@@ -0,0 +1,24 @@
+# x=str(input("enter the no: "))
+# print(int(x[0])**int(x[1]))
+
+# a=int(input('enter the principle amount: '))
+# b=int(input('rate of interest: '))
+# c=int(input('no. of time interest get coumpunded '))
+# t=int(input('no. of time:'))
+# i=(a+((1+b/c)**(c*t)))
+# print(i)
+
+# print("enter the center of the circle point: ")
+# x1=int(input("enter the x co-ordinate: "))
+# y1=int(input("enter the y co-ordinate: "))
+# r=int(input("enter the radius of the circle: "))
+# print("enter the point to search: ")
+# x2=int(input("enter the x co-ordinate: "))
+# y2=int(input("enter the y co-ordinate: "))
+# z=((x2-x1)**2 + (y2-y1)**2)**.5
+# if(z<r):
+#     print("inside the circle")
+# elif(z==r):
+#     print("on the circle")  
+# else:
+#     print("outside the circle")    
