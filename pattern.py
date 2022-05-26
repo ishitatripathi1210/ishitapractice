8accbd80e4ab5523604513b524025ae511d8d2a8
ishitatripathi1210 <106306979+ishitatripathi1210@users.noreply.github.com>
2022-05-26 22:31:49 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,65 @@
+# i=int(input("enter the no: "))
+# for a in range(1,i+1):
+#     print("*"*a)
+    
+# i=int(input("enter the no: "))
+# for a in range(i,0,-1):
+#     print("*"*a)
+    
+# i=int(input("enter the no: "))
+# for a in range(1,i+1):
+#     print((i-a)*" ","*"*a)
+    
+# i=int(input("enter the no: "))
+# for a in range(1,i+1):
+#     print((i-a)*" ","*"*a)
+ 
+# i=int(input("enter the no: "))
+# k=0
+# for a in range(i,0,-1):
+#     print((k)*" ","*"*a)
+#     k+=1
+    
+# i=int(input("enter the no: "))
+# k=0
+# for a in range(i,0,-1):
+#     print((k)*" ","* "*a)
+#     k+=1
+    
+# i=int(input("enter the no: "))
+# j=0
+# l=1
+# for a in range(1,i+1):
+#     print((i-a)*" ","*"*(a+j))
+#     j+=1
+# i-=1
+# for a in range(i,0,-1):
+#     print((l)*" ","*"*((a*2)-1))
+#     l+=1
+    
+    
+# n = int(input("enter the no: "))
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(j, end="")
+#     print()
+    
+    
+# n = int(input("enter the no: "))
+# k=1
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(k, end="")
+#         k+=1
+#     print()
+    
+    
+    
+# n=int(input("enter the no: "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if (j==1 or j==n or i==1 or i==n):
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
