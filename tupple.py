22b7e24bbf919aa6fa1bce2e4acd14289e5ee665
ishitatripathi1210 <106306979+ishitatripathi1210@users.noreply.github.com>
2022-05-26 22:34:14 +0530

    Create tupple.py

---
+++ tupple.py
@@ -0,0 +1,32 @@
+# tuple1 = (10, 20, 30, 40, 50)
+# print(tuple1[::-1])
+# tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+# print(tuple1[1][1])
+
+
+# t1=50,
+# tuple1 = (10, 20, 30, 40)
+# a,b,c,d = tuple1
+# b=60
+# tuple1=a,b,c,d
+# print(tuple1)
+
+# t= (99, 88)
+# t1=(11, 22)
+# t,t1=t1,t
+# print(t)
+# print(t1)
+
+# tuple1 = (11, [22, 33], 44, 55)
+# a,b,c,d=tuple1
+# b[0]=222
+# tuple1=a,b,c,d
+# print(tuple1)
+
+
+# tuple1 = (45, 45, 45, 45)
+# c='true'
+# for i in tuple1:
+#     if(tuple1[0]!=i):
+#            c='false'
+# print(c)
