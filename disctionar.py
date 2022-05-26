735fca2d187df2e016f7d1a0f6479eeea90a2440
ishitatripathi1210 <106306979+ishitatripathi1210@users.noreply.github.com>
2022-05-26 22:33:16 +0530

    Create disctionar.py

---
+++ disctionar.py
@@ -0,0 +1,25 @@
+# d={}
+# for i in range(2):
+#     k=(input())
+#     v=(input())
+#     d.update({k:v})
+# print(d)
+# d={1:2,2:3,3:4}
+# s=0
+# # for i in d:
+# #     s+=d[i]
+# # print(s)
+# # print(sum(d.values()))
+# d={1:2,2:3,3:4}
+# d1={4:5,5:6,6:7}
+# d3={}
+# for i in (d,d1):
+#     d3.update(i)
+# print(d3h)
+# def su(x):
+#     s=0
+#     for i in x:
+#         s+=i
+#     print(s)
+# l=[7,8,9]
+# su(l)
